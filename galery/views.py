8c80ea650001da94e20cd737eb712c86ba02b1ba
LeoSgarbi <lsgarbi1@gmail.com>
2023-04-17 20:03:23 -0300

    DRY Code

--- galery/views.py
+++ galery/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galery/index.html')
+
+def imagem(request):
+    return render(request, 'galery/imagem.html')
