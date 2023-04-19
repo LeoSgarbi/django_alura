9b412f3717a9b414d0b8799332b3a114fb117475
LeoSgarbi <lsgarbi1@gmail.com>
2023-04-19 19:56:53 -0300

    Config Django Admin

--- galery/admin.py
+++ galery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galery.models import Photos
 
-# Register your models here.
+class PhotosList(admin.ModelAdmin):
+    list_display = ("id", "name", "subtittle")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Photos, PhotosList)
--- galery/models.py
+++ galery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Photos(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     subtittle = models.CharField(max_length=150, null=False, blank=False)
-    description = models.TextField(null=False, blank=False)
+    description = models.TextField(null=False, blank=True)
     photo = models.CharField(max_length= 100, null=False, blank=False)
 
     def __str__(self):
