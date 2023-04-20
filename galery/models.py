e60a7b2690fefd3940de9bd2ec311c440b8b92b2
LeoSgarbi <lsgarbi1@gmail.com>
2023-04-20 11:47:53 -0300

    Including Category

--- galery/models.py
+++ galery/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Photos(models.Model):
+    category_options= [
+    ("PORTRAIT", "Portrait"),
+    ("B&W", "B&W"),
+    ("LANDSCAPE", "Landscape"),
+    ("PHOTOJOURNALISM", "Photojournalism"),
+    ]
     name = models.CharField(max_length=100, null=False, blank=False)
     subtittle = models.CharField(max_length=150, null=False, blank=False)
+    category = models.CharField(max_length=100, choices=category_options, default='')
     description = models.TextField(null=False, blank=True)
     photo = models.CharField(max_length= 100, null=False, blank=False)
 
