2921f04120c9abd66c34c215c1f145d51fc828d5
LeoSgarbi <lsgarbi1@gmail.com>
2023-05-03 10:46:54 -0300

    Activate searching mechanism

--- galery/models.py
+++ galery/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+
 from datetime import datetime
 
 class Photos(models.Model):
+
     category_options= [
-    ("PORTRAIT", "Portrait"),
-    ("B&W", "B&W"),
-    ("LANDSCAPE", "Landscape"),
-    ("PHOTOJOURNALISM", "Photojournalism"),
+        ("PORTRAIT", "Portrait"),
+        ("B&W", "B&W"),
+        ("LANDSCAPE", "Landscape"),
+        ("PHOTOJOURNALISM", "Photojournalism"),
     ]
+    
     name = models.CharField(max_length=100, null=False, blank=False)
     subtittle = models.CharField(max_length=150, null=False, blank=False)
     category = models.CharField(max_length=100, choices=category_options, default='')
     description = models.TextField(null=False, blank=True)
-    photo = models.CharField(max_length= 100, null=False, blank=False)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     published = models.BooleanField(default=False)
     photo_date = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"photography [nome={self.name}]"
+        return self.name
--- galery/urls.py
+++ galery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galery.views import index, imagem
+from galery.views import index, imagem, searching
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:photo_id>', imagem, name='imagem'),
+    path("searching", searching, name="searching")
 ]
