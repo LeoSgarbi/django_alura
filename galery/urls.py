7c68a5f87144eccacd753e6989c6836a1f945eea
LeoSgarbi <lsgarbi1@gmail.com>
2023-05-08 22:30:24 -0300

    deleting duplicates

--- galery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from galery.views import index, imagem, searching
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('imagem/<int:photo_id>', imagem, name='imagem'),
-    path("searching", searching, name="searching")
-]
