d5e065abf045b4c772160b830efe8773003cc562
LeoSgarbi <lsgarbi1@gmail.com>
2023-05-09 20:47:52 -0300

    validating data and passwords

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -24,6 +24,7 @@ class LoginForms(forms.Form):
         )
     )
 
+
 class CadastroForms(forms.Form):
     nome_cadastro=forms.CharField(
         label="Nome de Cadastro",
@@ -37,13 +38,35 @@ class CadastroForms(forms.Form):
         )
     )
     email=forms.EmailField(
-        label="Nome de Cadastro",
+        label="Email",
         required=True,
         max_length=100,
         widget=forms.EmailInput(
             attrs={
                 "class": "form-control",
                 "placeholder": "Ex: joaosilva@xpto.com"
+            }
+        )
+    )
+    senha_1=forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
             }
         )
     )
